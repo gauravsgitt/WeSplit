133532d6a4509efea1201699a4b3c095ad5c5764
Gaurav Bisht <gaurav.bisht@tothenew.com>
2024-09-03 12:41:41 +0530

    Added all UI components

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var numberOfPeople = 2
+    @State private var amount = 0.0
+    @State private var selectedTipPercentage = 0
+    @FocusState private var isAmountTextFieldFocued: Bool
+    
+    let tipPercentages = [0, 5, 10, 15, 20]
+    var tipAmount: Double { (Double(selectedTipPercentage) / 100) * amount }
+    var totalAmount: Double { amount + tipAmount }
+    var perPersonAmount: Double {
+        
+        return numberOfPeople == 0 ? 0.0 : (totalAmount / Double(numberOfPeople + 2))
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Amount and number of people") {
+                    TextField("", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                        .keyboardType(.decimalPad)
+                        .focused($isAmountTextFieldFocued)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $selectedTipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                } header: {
+                    Text("How much do you want to tip?")
+                        .textCase(.uppercase)
+                }
+                
+                Section("Amount per person") {
+                    Text(perPersonAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                }
+                
+                Section("total amount for the check") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if isAmountTextFieldFocued {
+                    Button("Done") {
+                        isAmountTextFieldFocued = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
